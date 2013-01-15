faa23fd532945ba3ae62b6ff2d192c954b84e267
jfrijters <jfrijters>
2013-01-15 10:28:01 +0000

    Updated version to 7.2.4630.6 and changed copyright year to 2013. Backported bug fixes: - Don't deadlock AppDomain.ProcessExit event handler when the event gets called from another thread than the one initiating exit. - Static compiler should not use proxy stubs to implement non-public interfaces in another assembly (because this situation can only arise when compiling the assemblies together and the InternalsVisibileToAttribute will be used and proxy stubs are not understood by CompiledTypeWrapper, so they can only be used in dynamic mode). - Don't add duplicate methods to attribute annotation interfaces. The primary cause of this was attributes with virtual properties where we would add the base class property as well as the derived class overridden property. - Local variable analysis for finally blocks was incorrect. Fixes bug #3600788.

--- openjdk/java/lang/Shutdown.java
+++ openjdk/java/lang/Shutdown.java
@@ -60,9 +60,6 @@ class Shutdown {
     // the index of the currently running shutdown hook to the hooks array
     private static int currentRunningHook = 0;
 
-    // [IKVM] have we registered the AppDomain.ProcessExit event handler?
-    private static boolean registeredProcessExit;
-
     /* The preceding static fields are protected by this lock */
     private static class Lock { };
     private static Object lock = new Lock();
@@ -77,7 +74,7 @@ class Shutdown {
         }
     }
     
-    private static void registerProcessExit() {
+    static {
         try {
             // MONOBUG Mono doesn't support starting a new thread during ProcessExit
             // (and application shutdown hooks are based on threads)
@@ -132,11 +129,6 @@ class Shutdown {
             } else {
                 if (state > HOOKS || (state == HOOKS && slot <= currentRunningHook))
                     throw new IllegalStateException("Shutdown in progress");
-            }
-
-            if (!registeredProcessExit) {
-                registeredProcessExit = true;
-                registerProcessExit();
             }
 
             hooks[slot] = hook;
